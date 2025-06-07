21566c59e8b8b4810b66f4d40ba9e08a3f0ac0d2
Oluwadare Oluwaseun <shecuzzy1@gmail.com>
2025-06-07 13:01:49 +0100

    Update SQL Query to create project database and schemas.sql

--- Scripts/SQL Query to create project database and schemas.sql
+++ Scripts/SQL Query to create project database and schemas.sql
@@ -6,7 +6,7 @@ Create Database and schemas
 /*
 Script's Purpose:
 This script helps create a new database called 'DataWarehouseAnalysis'. The script also sets up three schemas within the created database: 'bronze', 'silver', and 'gold'.
-WARNING: If a database with the name 'DataWarehouseAnalysis' already exists in the system, there will be an error
+WARNING: If a database with the name 'DataWarehouseAnalysis' already exists in the system, there will be an error message as there cannot be multiple instances of the same database.
 */
 
 

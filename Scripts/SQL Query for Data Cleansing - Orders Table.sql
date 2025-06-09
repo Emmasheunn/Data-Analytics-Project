99bc7242c3a6b82c91b6e54442e2d60061190611
Oluwadare Oluwaseun <shecuzzy1@gmail.com>
2025-06-09 03:43:26 +0100

    Update SQL Query for Data Cleansing - Orders Table.sql

--- Scripts/SQL Query for Data Cleansing - Orders Table.sql
+++ Scripts/SQL Query for Data Cleansing - Orders Table.sql
@@ -1,120 +1,97 @@
 /*
------------------------------------------------------------------
+-----------------------------------------------------
 Data Cleansing - Orders Table
------------------------------------------------------------------
+-----------------------------------------------------
 Objective:
-This scripts handles all nulls according to project requirements.
-These fixes were effected only after careful checks on each column.
+This script handles data cleansing for orders based on project rules.
 Fixes include:
-- Handling duplicate values in Primary key
-- Drop nonsensical values in measure columns
-- Cleaned Price column from products table
-- Recalculated TotalAmount column with price from products table
-- Handling date format inconsistencies
-- Handling NULLS
-The cleaned table is then inserted into the silver.Orders table.
+- Removing duplicates in primary key (OrderID)
+- Cleaning inconsistent date formats
+- Removing invalid quantity and price values
+- Recalculating TotalAmount using product prices
+- Replacing NULLs with defaults
+- Inserting cleaned data into silver.Orders table
 */
 
-wITH CTE_Cleaned AS 
-(	SELECT 
-		OrderID,
-		CustomerID,
-		ProductID,
-		OrderDate,
-		Quantity,
-		TotalAmount
-	FROM 
-	(
-			SELECT 
-				OrderID,
-				CustomerID,
-				ProductID,
-				-- Fixing date inconsistencies and handling nulls
-				CASE 
-					WHEN ISDATE(OrderDate) = 1 THEN CAST(OrderDate AS DATE)
-					WHEN OrderDate IS NULL THEN CAST('1900-01-01' AS DATE)
-					WHEN ISDATE(CONCAT(RIGHT(OrderDate,4),'-',
-						SUBSTRING(OrderDate,4,2),'-',LEFT(OrderDate,2))) = 1 THEN 
-						CAST(CONCAT(RIGHT(OrderDate,4),'-',
-						SUBSTRING(OrderDate,4,2),'-',LEFT(OrderDate,2)) AS DATE)
-					ELSE CAST('2055-01-01' AS DATE)
-				END AS OrderDate,
-				Quantity,
-				TotalAmount
-			FROM (
-				SELECT 
-					*,
-					ROW_NUMBER() OVER (PARTITION BY OrderID ORDER BY OrderID) AS number
-				FROM bronze.stg_orders)t
-			WHERE number = 1 -- Fixing duplicates in Primary Key by selecting only distinct OrderID
-	)t
-	WHERE Quantity > 0		-- Fixing Quantity and TotalAmount columns by dropping 
-							-- orders with negative values and TotalAmounts that are not numbers
+WITH CTE_DeduplicatedOrders AS (
+    SELECT
+        OrderID,
+        CustomerID,
+        ProductID,
+        -- Fix date inconsistencies and replace NULLs with placeholders
+        CASE 
+            WHEN ISDATE(OrderDate) = 1 THEN CAST(OrderDate AS DATE)
+            WHEN OrderDate IS NULL THEN CAST('1900-01-01' AS DATE)
+            WHEN ISDATE(CONCAT(RIGHT(OrderDate,4),'-',SUBSTRING(OrderDate,4,2),'-',LEFT(OrderDate,2))) = 1 
+                THEN CAST(CONCAT(RIGHT(OrderDate,4),'-',SUBSTRING(OrderDate,4,2),'-',LEFT(OrderDate,2)) AS DATE)
+            ELSE CAST('2055-01-01' AS DATE)
+        END AS OrderDate,
+        Quantity,
+        TotalAmount
+    FROM (
+        SELECT *,
+            ROW_NUMBER() OVER (PARTITION BY OrderID ORDER BY OrderID) AS rn
+        FROM bronze.stg_orders
+    ) AS t
+    WHERE rn = 1 AND Quantity > 0 -- Keep only the first occurrence and valid quantities
 ),
-CTE_PriceFromProducts AS (
-	SELECT 
-		ProductID,
-		Price
-	FROM bronze.stg_products
-	WHERE Price != 'invalid'
+
+CTE_ValidPrices AS (
+    SELECT 
+        ProductID,
+        CAST(Price AS FLOAT) AS Price
+    FROM bronze.stg_products
+    WHERE ISNUMERIC(Price) = 1
 ),
- CTE_Collect AS (
-	SELECT distinct
-		CO.OrderID,
-		CO.CustomerID,
-		CO.ProductID,
-		CO.OrderDate,
-		CO.Quantity,
-	-- Recalculating TotalAmount AS Quantity * Price and casting back to NVARCHAR to fit silver layer table
-		CAST(ROUND(CAST(CO.Quantity AS float) * CAST(P.Price AS float),2) AS NVARCHAR) AS TotalAmount
-	FROM CTE_Cleaned AS CO
-	 LEFT JOIN CTE_PriceFromProducts AS P
-	ON P.ProductID = CO.ProductID
-	-- Dropping all orders that don't have a price
-	WHERE P.Price IS NOT NULL
+
+CTE_CleanedOrders AS (
+    SELECT
+        o.OrderID,
+        o.CustomerID,
+        o.ProductID,
+        o.OrderDate,
+        o.Quantity,
+        -- Recalculate TotalAmount using validated price
+        CAST(ROUND(o.Quantity * p.Price, 2) AS NVARCHAR) AS TotalAmount
+    FROM CTE_DeduplicatedOrders o
+    LEFT JOIN CTE_ValidPrices p ON o.ProductID = p.ProductID
+    WHERE p.Price IS NOT NULL -- Exclude products without valid prices
 )
 
-	-- Collecting all cleaned columns
+-- Insert cleaned records into the silver layer
 INSERT INTO silver.Orders (
-							OrderID,
-							CustomerID,
-							ProductID,
-							OrderDate,
-							Quantity,
-							TotalAmount
-						  )
+    OrderID,
+    CustomerID,
+    ProductID,
+    OrderDate,
+    Quantity,
+    TotalAmount
+)
+SELECT
+    OrderID,
+    CustomerID,
+    ProductID,
+    OrderDate,
+    Quantity,
+    TotalAmount
+FROM CTE_CleanedOrders;
 
-	SELECT 
-		OrderID,
-		CustomerID,
-		ProductID,
-		OrderDate,
-		Quantity,
-		TotalAmount
-	FROM CTE_Collect
+-- Post-validation checks
+-- Check for duplicates or NULL OrderIDs
+SELECT * FROM (
+    SELECT *,
+        ROW_NUMBER() OVER (PARTITION BY OrderID ORDER BY OrderID) AS dup_count
+    FROM silver.Orders
+) AS t
+WHERE dup_count > 1 OR OrderID IS NULL;
 
--- Checking if data quality fixes were effective in silver layer
-SELECT *
-FROM
-	(SELECT 
-		*,
-		ROW_NUMBER() OVER (PARTITION BY OrderID ORDER BY OrderID) AS counts
-	FROM silver.Orders)t
-WHERE counts > 1 OR OrderID IS NULL;
+-- Check for missing foreign keys
+SELECT * FROM silver.Orders WHERE CustomerID IS NULL;
+SELECT * FROM silver.Orders WHERE ProductID IS NULL;
 
--- Checking foreign key for nulls
-SELECT * FROM silver.Orders
-WHERE CustomerID IS NULL;
-
-SELECT * FROM silver.Orders
-WHERE ProductID IS NULL;
-
--- Checking OrderDate column for date inconsistencies
-SELECT 
-	OrderDate,
-	ISDATE(OrderDate) AS verify
+-- Validate OrderDate
+SELECT OrderDate, ISDATE(OrderDate) AS is_valid_date
 FROM silver.Orders;
 
--- Checking for nonsensical values in quantity field
-SELECT * FROM silver.Orders
-WHERE Quantity < 1
+-- Validate Quantity
+SELECT * FROM silver.Orders WHERE Quantity < 1;
